18fffac376702388e15bf1dc2f5eeb2ad3e30b48
pokemau <rentillosa90@gmail.com>
2024-09-09 07:05:12 +0800

    z

--- distro/arch/install.sh
+++ distro/arch/install.sh
@@ -14,16 +14,28 @@ brightnessctl grimblast-git cliphist pamixer pavucontrol \
 network-manager-applet blueman udiskie ttf-jetbrains-mono-nerd \
 ttf-hack-nerd ttf-meslo-nerd yarn npm qt5-quickcontrols qt5-quickcontrols2 \
 qt5-graphiacaleffects slurp xdg-desktop-portal-hyprland nwg-look qt5ct \
-qt6ct kvantum kvantum-qt5 qt5-wayland qt6-wayland thunar github-cli\
+qt6ct kvantum kvantum-qt5 qt5-wayland qt6-wayland thunar github-cli \
 thunar-archive-plugin ark discord pipewire pipewire-alsa pipewire-audio \
 pipewire-jack pipewire-pulse gst-plugin-pipewire wireplumber pavucontrol \
 tmux zsh vlc qbittorrent unzip flatpak wlsunset rust xdg-desktop-portal \
 hyprpicker-git noto-fonts bear ripgrep baobab gnome-calculator stow \
 glfw ttf-ms-win11-auto xdg-desktop-portal-gtk ttf-cascadia-code-nerd \
 ttf-cascadia-code ttf-roboto-mono-nerd nerd-fonts-sf-mono nwg-displays \
-brave-bin
+visual-studio-code-bin gvfs noto-fonts-cjk noto-fonts-emoji gvfs
+
+yay -S gnome-themes-extra
 
 # uncomment multilib in pacman.conf
+
+# KDE Plasma
+# plasma-desktop plasma-nm plasma-pa
+#
+# Gnome
+# gnome gnome-browser-connector
+# nautilus-open-any-terminal
+
+flatpak install flathub com.github.IsmaelMartinez.teams_for_linux && \
+flatpak install flathub com.spotify.Client
 
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme
 ./Tela-circle-icon-theme/install.sh
--- stow.sh
+++ stow.sh
@@ -2,11 +2,14 @@
 
 distro=$1
 
+rm -rf ~/.zshrc
+
 stow alacritty dunst kitty nvim rofi tmux xfce4 zsh zed waybar \
     clang-format
 
 if [ "$distro" = "hyprland" ]; then
     echo "ARCH"
+    rm -rf ~/.config/hypr
     stow hypr swappy scripts
 elif [ "$distro" = "sway" ]; then
     echo "SWAY"
