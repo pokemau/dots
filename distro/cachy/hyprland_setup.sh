9e90f47c8e86995be89afc0ca0986b202a4a1886
pokemau <rentillosa90@gmail.com>
2026-07-05 14:36:12 +0800

    setup

--- distro/cachy/hyprland_setup.sh
+++ distro/cachy/hyprland_setup.sh
@@ -11,7 +11,7 @@ cd yay && makepkg -si
 yay -S stow discord spotify-launcher power-profiles-daemon flatpak wlsunset nautilus \
     network-manager-applet blueman pavucontrol nwg-look brightnessctl pipewire wireplumber \
     xdg-desktop-portal-hyprland hyprpolkitagent hyprpaper grimblast-git qt5ct qt6ct \
-    gnome-calculator baobab swaync obs-studio adw-gtk-theme qbittorrent pulseaudio\
+    gnome-calculator baobab swaync obs-studio adw-gtk-theme qbittorrent \
     tela-circle-icon-theme-standard kvantum kvantum-qt5 fzf pipewire-pulse swappy \
     wget
 
@@ -36,7 +36,7 @@ yay -S git visual-studio-code-bin android-studio tmux zsh zed wl-clipboard xclip
 ##################
 ## STOW CONFIGS ##
 ##################
-rm -rf ~/.config/hypr ~/.zshrc ~/.tmux.conf
+rm -rf ~/.config/hypr/ ~/.zshrc ~/.tmux.conf ~/.config/swappy/
 cd ~/dots/configs && stow alacritty clang-format ghostty hypr kitty nvim quickshell scripts tmux wallpapers waybar zed zsh swappy \
     -t ~
 
@@ -52,5 +52,8 @@ cd Graphite-gtk-theme
 
 systemctl enable power-profiles-daemon.service
 systemctl start power-profiles-daemon.service
+
+### TODO
+# append [Appearance] icon_theme=Tela-circle-dark in .config/qt5/6-ct
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
